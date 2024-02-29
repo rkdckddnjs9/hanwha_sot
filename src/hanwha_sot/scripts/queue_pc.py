7e03232a011fb2c86a5d8761cbf9189c60400fdf
rkdckddnjs9 <105358882+rkdckddnjs9@users.noreply.github.com>
2024-03-01 01:24:50 +0900

    Update queue_pc.py

--- src/hanwha_sot/scripts/queue_pc.py
+++ src/hanwha_sot/scripts/queue_pc.py
@@ -19,17 +19,18 @@ class PointCloudProcessor:
             self.receive_from_ros_custom(msg)
             self.queue.task_done()
 
-    def receive_from_ros_custom(self, msg):
-        cloud_array = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
-        x_cond = (cloud_array['x'] >= 0) & (cloud_array['x'] <= 57)
-        y_cond = (cloud_array['y'] >= -44.8) & (cloud_array['y'] <= 44.8)
-        # z_cond = (cloud_array['z'] >= -2) & (cloud_array['z'] <= 4)
-        z_cond = (cloud_array['z'] >= -3) & (cloud_array['z'] <= 3)
+    def receive_from_ros_custom(self, msg): #self.point_cloud_range = [0, -44.8, -3, 57.6, 44.8, 3]
+        cloud_array = np.copy(ros_numpy.point_cloud2.pointcloud2_to_array(msg))
+        cloud_array['z'] *= -1
+        cloud_array['y'] *= -1
+        
+        x_cond = (cloud_array['x'] >= 0) & (cloud_array['x'] <= 57.599)
+        y_cond = (cloud_array['y'] >= -44.799) & (cloud_array['y'] <= 44.799)
+        z_cond = (cloud_array['z'] >= -1.999) & (cloud_array['z'] <= 3.999)
+        # z_cond = (cloud_array['z'] >= -3) & (cloud_array['z'] <= 3)
 
         filtered_array = cloud_array[x_cond & y_cond & z_cond]
 
-        filtered_array['z'] *= -1
-        filtered_array['y'] *= -1
         
         # modified_cloud = ros_numpy.point_cloud2.array_to_pointcloud2(filtered_array, stamp=rospy.Time.now(), frame_id=msg.header.frame_id)
         modified_cloud = ros_numpy.point_cloud2.array_to_pointcloud2(filtered_array, frame_id=msg.header.frame_id)
